f6f279e77a037e103c6e65276fc9cd14cbcfb8aa
JAY4420 <jaymovaliya14@gmail.com>
2026-03-27 20:52:33 +0530

    all setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-center bg-blue-500 text-white p-4">
+        Tailwind is working 🚀
+      </div>
+    </>
+  )
+}
+
+export default App
